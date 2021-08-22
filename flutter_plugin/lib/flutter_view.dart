704849975c6415185e60473a0ad4e0d2b9482e5a
sleticalboy <sleticalboy@gmail.com>
2021-08-22 10:02:47 +0800

    [feat]Infinate list view via flutter

--- flutter_plugin/lib/flutter_view.dart
+++ flutter_plugin/lib/flutter_view.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:ffi';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -16,14 +17,32 @@ class FlutterViewApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     log("build called");
-    return const MaterialApp(
+    return MaterialApp(
       title: "FlutterView",
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          title: const Text("FlutterView"),
+          backgroundColor: Colors.green,
+        ),
+        body: const Center(
           child: Text(
             "FlutterView",
             style: TextStyle(color: Colors.black),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: "聊天",
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.contacts), label: "联系人"),
+            BottomNavigationBarItem(icon: Icon(Icons.circle), label: "朋友圈"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "设置"),
+          ],
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          backgroundColor: Colors.blue,
         ),
       ),
     );
